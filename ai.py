814dde02771549b083a92093eaa1203988b9e7c9
christopher simmons <kadensdad93@gmail.com>
2024-01-13 11:01:48 -0800

    .

--- ai.py
+++ ai.py
@@ -45,47 +45,48 @@ async def run_search(question):
         db_name=use_database,
         collection_name="lore",
         )
+    
+    if loader:
+        docs = await loader.aload()
+        # print(len(docs))
 
-    docs = await loader.aload()
+        template = """The context given is from our Dungeons & Dragons campaign, which is set in a world called Tungra. All of the context is important to the story and development of the characters, the locations, the world, and the plot. You will answer like the Dungeon Master.
+        If you don't know the answer, say that the answer has not been added to the campaign lore, don't try to make up an answer.
+        Use three sentences maximum and keep the answer as concise as possible.
+        Always say "thanks for asking!" at the end of the answer.
 
-    # print(len(docs))
+        {context}
 
-    template = """The context given is from our Dungeons & Dragons campaign, which is set in a world called Tungra. All of the context is important to the story and development of the characters, the locations, the world, and the plot. You will answer like the Dungeon Master.
-    If you don't know the answer, say that the answer has not been added to the campaign lore, don't try to make up an answer.
-    Use three sentences maximum and keep the answer as concise as possible.
-    Always say "thanks for asking!" at the end of the answer.
+        Question: {question}
 
-    {context}
+        Helpful Answer:"""
+        custom_rag_prompt = PromptTemplate.from_template(template)
 
-    Question: {question}
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=500)
+        splits = text_splitter.split_documents(docs)
+        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-    Helpful Answer:"""
-    custom_rag_prompt = PromptTemplate.from_template(template)
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=500)
-    splits = text_splitter.split_documents(docs)
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-
-    # Retrieve and generate using the relevant snippets of the blog.
-    retriever = vectorstore.as_retriever()
-    # prompt = hub.pull("rlm/rag-prompt")
-    llm = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0)
-
-
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
+        # Retrieve and generate using the relevant snippets of the blog.
+        retriever = vectorstore.as_retriever()
+        # prompt = hub.pull("rlm/rag-prompt")
+        llm = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0)
 
 
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | custom_rag_prompt
-        | llm
-        | StrOutputParser()
-    )
-    message = rag_chain.invoke(question)
+        def format_docs(docs):
+            return "\n\n".join(doc.page_content for doc in docs)
 
-    return message
 
+        rag_chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | custom_rag_prompt
+            | llm
+            | StrOutputParser()
+        )
+        message = rag_chain.invoke(question)
+
+        return message
+    else:
+        return "There is nothing even remotely close to this in the campaign lore, dummy."
 
 
 
